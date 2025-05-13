17cf7fb44b10be6f3d43599ab0c25dbf733b22a3
Jerry Yao <99371069+Jerry-0215@users.noreply.github.com>
2025-05-13 09:17:14 -0400

    Added Function laass

---
+++ Function.java
@@ -0,0 +1,27 @@
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+public class Function {
+    private final Expression expression;
+
+    //Constructor given an expression string
+    public Function(String expression) {
+        this.expression = new ExpressionBuilder(expression).variable("x").build();
+    }
+
+    /**
+     * Evaluate the function at a given x value
+     * @param x
+     * @return
+     */
+    public double evaluate(double x) {
+        return expression.setVariable("x", x).evaluate();
+    }
+
+    /**
+     * Get the expression string
+     * @return
+     */
+    public Expression getExpression() {
+        return expression;
+}
